a0aa14c8f952530cb9e9f58652b6360a4550e8d2
Muhammad Haram <haramh643@gmail.com>
2023-12-25 00:36:40 +0500

    add shop page

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
-const close = document.getElementById('close');
-const bar = document.getElementById('bar');
-const nav = document.getElementById('navbar');
+// home page js
 
-console.log(bar)
-console.log(nav)
-console.log(close)
+const close = document.getElementById("close");
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
 
-if(bar){
-    bar.addEventListener('click', ()=>{
-        nav.classList.add('active');
-    })
+console.log(bar);
+console.log(nav);
+console.log(close);
+
+if (bar) {
+    bar.addEventListener("click", () => {
+        nav.classList.add("active");
+    });
 }
 
-if(close){
-    close.addEventListener('click', ()=>{
-        nav.classList.remove('active');
-    })
+if (close) {
+    close.addEventListener("click", () => {
+        nav.classList.remove("active");
+    });
+}
+
+
+// single product page js
+
+var MainImg = document.getElementById("MainImg");
+var smallImages = document.getElementsByClassName("small-img");
+
+smallImages[0].onclick = function(){
+    MainImg.src = smallImages[0].src;
+}
+
+smallImages[1].onclick = function(){
+    MainImg.src = smallImages[1].src;
+}
+
+smallImages[2].onclick = function(){
+    MainImg.src = smallImages[2].src;
+}
+
+smallImages[3].onclick = function(){
+    MainImg.src = smallImages[3].src;
 }
